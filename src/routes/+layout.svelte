bfc2a176fe142ad9d47af973125cbd41eff1ef06
Frank Renberg <fraren@elevad.umea.se>
2025-04-02 14:16:07 +0200

    message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script>
 	import '../app.postcss';
-
+	import {base} from '$app/paths';
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
 	import 'highlight.js/styles/github-dark.css';
@@ -22,4 +22,58 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 </script>
 
+<nav class="navbar">
+	<a href="{base}/">Home</a>
+	<a href="{base}/Items">Items</a>
+	<a href="{base}/Building">Building</a>
+	<a href="{base}/Guides">Guides</a>
+
+</nav>
+<input class="search" type="text" placeholder="Search">
+<main>
+	
+</main>
+
+
 <slot />
+<style>
+main{
+	display: flex;
+	flex-direction: row;
+	width: 100vw;
+	height: 100vh;
+	background-color: #0f0e14;
+}
+.navbar{
+	display:grid;
+	position: fixed;
+	top: 0;	
+	justify-content: space-around;
+	grid-template-columns: 1fr 4fr,4fr,4fr;
+	grid-row: 1fr;
+	grid-auto-flow: column;
+	background-color: #1f1c2b;
+	color: rgb(135, 58, 0);
+	padding: 10px;
+	width: 50%;
+	border-bottom: #717171 2px solid;
+	border-right: #717171 2px solid;
+	border-bottom-right-radius: 10px;
+	
+}
+.search{
+	position: fixed;
+	top: 0;
+	right: 0;
+	width: 25%;
+	padding: 10px;
+	border: none;
+	background-color: #1f1c2b;
+	color : rgb(135, 58, 0);
+	border-bottom: #717171 2px solid;
+	border-left: #717171 2px solid;
+	border-bottom-left-radius: 10px;
+}
+
+</style>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,33 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
-	</div>
-</div>
+</script>
+
+
+<section class="recent">
+	<h1 style="justify-self: center; align-self: center; font-size:xx-large;order-bottom: #717171 1px solid;padding: 20px;width: 100%;text-align: center; border-bottom:solid 1px #717171;color:rgb(135, 58, 0)" >Recent</h1>
+	<!-- Lägg till senaste 3 searches -->
+</section>
+
+
+
+<style>
+.recent{
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 1fr 2fr 2fr 2fr;
+	background-color:#1f1c2b;
+	border: 1px solid #717171;
+	position:absolute;
+	top: 35%;
+	left: 20%;
+	width: 60%;
+	height: 60%;
+	border-top-left-radius: 20px;
+	border-top-right-radius: 20px;
+	
+
+}
+#recent{
+
+}
+</style>
