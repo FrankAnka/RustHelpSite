88ecb96531adf9e56ec058e662af5021000e5e97
Frank Renberg <fraren@elevad.umea.se>
2025-05-07 14:15:36 +0200

    devblog 2

---
+++ docs/_app/immutable/chunks/CTIgZt-s.js
@@ -0,0 +1,11 @@
+import{s as R,n as g}from"./BWfIk2MJ.js";import{S as k,i as q,d as n,b as l,c as h,g as r,e as p,h as m,s as u}from"./Bp1yjbIX.js";function z(P){let i,w="Hello",f,s,H=`This is the second devblog on this project and since last time ive changed the way i style from using pure css to npw using premade elements from daisy.ui.
+I have also pretty much completed the item page, i added explosives, added all the data and made sure it was displayed correctly. Currently the data is a bit off because i cannot find correct ones online and i need to test for my self.
+I also added a raid helper thing that shows you how much it would take to raid something.
+I also revamped the styling of the entire page.`,c,a,j="Changes",v,o,S="Features",x,d,I=`✅List Of buildings and information
+✅Generall Styling for most objects
+✅Navigation and general Menu setup
+✅Raidhelp
+✅Crafting recipes for items
+✅Popup with information
+✅Search
+✅`,C,y,_,b;return{c(){i=m("h1"),i.textContent=w,f=u(),s=m("p"),s.textContent=H,c=u(),a=m("h2"),a.textContent=j,v=u(),o=m("h2"),o.textContent=S,x=u(),d=m("p"),d.textContent=I,C=u(),y=m("hr"),_=u(),b=m("br")},l(t){i=h(t,"H1",{"data-svelte-h":!0}),r(i)!=="svelte-ggif4a"&&(i.textContent=w),f=p(t),s=h(t,"P",{"data-svelte-h":!0}),r(s)!=="svelte-1fjyk4d"&&(s.textContent=H),c=p(t),a=h(t,"H2",{"data-svelte-h":!0}),r(a)!=="svelte-6y66jh"&&(a.textContent=j),v=p(t),o=h(t,"H2",{"data-svelte-h":!0}),r(o)!=="svelte-mhy7zl"&&(o.textContent=S),x=p(t),d=h(t,"P",{"data-svelte-h":!0}),r(d)!=="svelte-u853i5"&&(d.textContent=I),C=p(t),y=h(t,"HR",{}),_=p(t),b=h(t,"BR",{})},m(t,e){l(t,i,e),l(t,f,e),l(t,s,e),l(t,c,e),l(t,a,e),l(t,v,e),l(t,o,e),l(t,x,e),l(t,d,e),l(t,C,e),l(t,y,e),l(t,_,e),l(t,b,e)},p:g,i:g,o:g,d(t){t&&(n(i),n(f),n(s),n(c),n(a),n(v),n(o),n(x),n(d),n(C),n(y),n(_),n(b))}}}const B={title:"Version 0.0.2",date:"2025-05-07"},{title:G,date:L}=B;class M extends k{constructor(i){super(),q(this,i,null,z,R,{})}}export{M as default,B as metadata};
